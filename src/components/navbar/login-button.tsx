7655dc54a94cb77fa9adddc20d20cde13de4c976
subhash131 <subhashnayak131@gmail.com>
2024-12-15 05:13:07 +0530

    update login button

--- src/components/navbar/login-button.tsx
+++ src/components/navbar/login-button.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import React from "react";
 import { RiLoginCircleFill } from "react-icons/ri";
 
-const SigninButton = () => {
+const LoginButton = () => {
   return (
     <Link href="#" className="px-6 py-1 flex items-center gap-1">
       <RiLoginCircleFill />
@@ -11,4 +11,4 @@ const SigninButton = () => {
   );
 };
 
-export default SigninButton;
+export default LoginButton;
