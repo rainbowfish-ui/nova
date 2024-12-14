883f6190b7a9d3dc6b8809b0e64f01182b12e19b
subhash131 <subhashnayak131@gmail.com>
2024-12-15 05:25:53 +0530

    update hero section

---
+++ src/components/landing-page/hero/heading.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div className="min-h-64 size-full flex items-center justify-center">
+      <h2 className="text-6xl text-center leading-tight">
+        A place to display your <br /> masterpiece
+      </h2>
+    </div>
+  );
+};
+
+export default Heading;
---
+++ src/components/landing-page/hero/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./heading";
+import Cards from "./cards";
+
+const Hero = () => {
+  return (
+    <div className="size-full">
+      <Heading />
+      <Cards />
+    </div>
+  );
+};
+
+export default Hero;
