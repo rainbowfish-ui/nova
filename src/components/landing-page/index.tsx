2941e4e3bc113e5c9456ad6c002cbc7901ef3da1
subhash131 <subhashnayak131@gmail.com>
2024-12-15 04:58:39 +0530

    setup layout

---
+++ src/components/landing-page/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div className="w-full min-h-screen flex items-center justify-start flex-col">
+      LandingPage
+    </div>
+  );
+};
+
+export default LandingPage;
